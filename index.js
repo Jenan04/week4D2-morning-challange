6771dc5a1578a21f62e70d7155f4f95a317f9a74
Jenan04 <jenanyasser1@gmail.com>
2025-08-03 21:55:17 +0300

    refactor

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 (function () {
-  function handleErr(err) {
+  const  handleErr = (err) => {
     var h1 = document.createElement('h1');
     h1.innerHTML = 'Server Error';
     document.querySelector('#repos').appendChild(h1);
   }
 
-  function renderRepos(repos) {
-    var view_capitalised = view.toUpperCase();
-    var other_view = view === 'fac' ? 'dwyl' : 'fac';
-    var other_view_capitalised = other_view.toUpperCase();
+  const renderRepos = (repos) =>{
+    const view_capitalised = view.toUpperCase();
+    const other_view = view === 'fac' ? 'dwyl' : 'fac';
+    const other_view_capitalised = other_view.toUpperCase();
 
-    var h1 = document.createElement('h1');
+    const h1 = document.createElement('h1');
     h1.innerHTML = view_capitalised + ' Repositories';
     document.querySelector('#repos').appendChild(h1);
 
-    var link = document.querySelector('#link');
+    const link = document.querySelector('#link');
     link.href = '/' + other_view;
     link.innerHTML = 'See ' + other_view_capitalised + '\'s repositories';
 
     repos.forEach(function (repo, i) {
-      var h3 = document.createElement('h3');
-      var a = document.createElement('a');
+      const h3 = document.createElement('h3');
+      const a = document.createElement('a');
       h3.innerHTML = (i + 1) + '. ' + repo.name;
       a.href = repo.url;
       a.appendChild(h3);
